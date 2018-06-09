89c81d3138c1d71ccf3a27a486171260ca619923
vivek <vvsharma25@gmail.com>
2018-06-10 01:21:16 +0200

    cleanup

--- index.js
+++ index.js
@@ -21,12 +21,7 @@ var port = process.env.PORT || 1337;
 app.listen(port);
 app.use(express.static(__dirname + '/public'));
 console.log("Server running at http://localhost:%d", port);
-// app.get('/api', (req, res) => res.json({ application: 'Reibo collection'}));
-// Redirect / to our html file
-// app.get('*', (req, res) =>  res.sendFile("index.html", {"root": __dirname}));
-
 app.get('*', function (req, res) {
- // res.type('text/html');
     res.sendFile("./public/index.html");
 
 });
