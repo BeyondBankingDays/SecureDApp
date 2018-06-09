4c890cc8f647d68ded7c7413bd3dbf9d254b7239
vivek <vvsharma25@gmail.com>
2018-06-09 15:37:22 +0200

    ng-patten issue : pattern might be a problme ,quick fix applied

--- public/pages/registration/registration.js
+++ public/pages/registration/registration.js
@@ -1,27 +1,28 @@
-angular.module('app.registrationController',[])
-    .controller('registrationController',[ '$scope','$http','$location','$timeout', function($scope, $http, $location, $timeout){
+angular.module('app.registrationController', [])
+    .controller('registrationController', ['$scope', '$http', '$location', '$timeout', function ($scope, $http, $location, $timeout) {
 
-        $scope.regObj = {password:'',
-    username:'',
-last_name:'',
-first_name:'',
-email:''};
+        $scope.regObj = {
+            password: '',
+            username: '',
+            last_name: '',
+            first_name: '',
+            email: ''
+        };
         $scope.reg = 'pending';
 
-        $scope.registerUser = function(){
-            console.log($scope.regObj.password);
-            $scope.regObj.password=$scope.regObj.password.toString();
-            $http.post('https://beyondbanking.openbankproject.com/obp/v2.0.0/users',JSON.stringify($scope.regObj)).then(function(success){
+        $scope.registerUser = function () {
+            $scope.regObj.password = $scope.regObj.password.toString();
+            $http.post('https://beyondbanking.openbankproject.com/obp/v2.0.0/users', JSON.stringify($scope.regObj)).then(function (success) {
                 console.log(success.data);
                 $scope.reg = 'success';
                 $scope.newUsername = success.data.username;
-                $timeout( function(){
+                $timeout(function () {
                     $location.path('/homepage');
-                }, 2000 );
-            }, function(error){
+                }, 2000);
+            }, function (error) {
                 console.log(error.data);
                 $scope.reg = 'error';
             })
         }
 
-}]);
+    }]);
