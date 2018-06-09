7fd2641870a0ce262dd6c9e5bc6be9462f73a7b4
saugandhkaran <saugandhkaran@gmail.com>
2018-06-09 04:15:41 +0200

    User details service calls

--- pages/profile/profile.js
+++ pages/profile/profile.js
@@ -1,4 +1,8 @@
-app.controller('profileController', function(){
+app.controller('profileController', ['$scope','$http', function($scope, $http){
+    $http.get('https://securedapi1.azurewebsites.net/user/4f432897-6c6c-4e29-a9b8-30c469799354').then(function(response){
+        $scope.name = response.data.name;
+        $scope.dob = response.data.dob;
+    })
 
-
-});
+    $scope.profile = {};
+}]);
