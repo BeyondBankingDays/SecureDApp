8767bf44729b85d1821472bdfe6cb54de9ff801e
vivek <vvsharma25@gmail.com>
2018-06-09 14:53:19 +0200

    MUltiple upload done

---
+++ public/pages/sucess/sucess.js
@@ -0,0 +1,8 @@
+angular.module('app.successController', [])
+    .controller('addController', ['$scope', '$http',
+        function ($scope, $http) {
+alert('successalertpage');
+
+success
+
+        }]);
