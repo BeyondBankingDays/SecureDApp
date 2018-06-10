2b1d767bf89f0dd571240a9c90e33bcfd36ea8fd
N44340 <gautam.lihala@nl.abnamro.com>
2018-06-10 02:33:55 +0200

    Removed comments

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-/*
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-   /!* response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");*!/
-
-});
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
-*/
 var express  = require('express');
 var path = require('path');
-var app      = express();
-// Define the port
+var app  = express();
 var port = process.env.PORT || 1337;
 app.listen(port);
 app.use(express.static(__dirname + '/public'));
 console.log("Server running at http://localhost:%d", port);
 app.get('*', function (req, res) {
     res.sendFile("./public/index.html");
-
 });
